d3bc5c81fd8cbeb9b79d82017e0a7dbc41aada68
sarath srinivas s <srinix@protonmail.com>
2018-06-08 03:04:22 +0530

    Added get_var_mat and gpr_predict. Using atlas blas libs.

--- lib_gpr.c
+++ lib_gpr.c
@@ -4,24 +4,21 @@
 #include <math.h>
 #include "lib_gpr.h"
 
-extern void dposv_(const unsigned char *UPLO, const int *N, const int *NRHS, double *A, const int *LDA,
-		   double *B, const int *LDB, int *info);
-
-int get_krn_se(double *krn, const double *x, const double *xp, unsigned long ns, unsigned long dim,
-	       const double *p, int np)
+int get_krn_se(double *krn, const double *x, const double *xp, unsigned long nx, unsigned long nxp,
+	       unsigned long dim, const double *p, int npar)
 {
-	double sig_y, sig_n, l, l2, r2, x_xp;
+	double sig_y, sig_n, l, l2, r2, x_xp, noise;
 	unsigned long i, j, k;
 
-	assert(np == 3);
+	assert(npar == 3);
 
 	sig_y = p[0];
 	sig_n = p[1];
 	l = p[2];
 	l2 = l * l;
 
-	for (i = 0; i < ns; i++)
-		for (j = 0; j < ns; j++) {
+	for (i = 0; i < nx; i++)
+		for (j = 0; j < nxp; j++) {
 
 			r2 = 0;
 			for (k = 0; k < dim; k++) {
@@ -29,8 +26,12 @@ int get_krn_se(double *krn, const double *x, const double *xp, unsigned long ns,
 				r2 += x_xp * x_xp;
 			}
 
-			krn[i * ns + j] = sig_y * sig_y * exp(-0.5 * r2 / l2);
+			krn[i * nxp + j] = sig_y * sig_y * exp(-0.5 * r2 / l2);
 		}
+
+	for (i = 0; i < nx; i++) {
+		krn[i * nxp + i] += sig_n * sig_n;
+	}
 
 	return 0;
 }
@@ -63,6 +64,76 @@ int get_gpr_weights(double *wt, double *krn_chd, const double *krn, unsigned lon
 	LDB = (int)ns;
 	NRHS = 1;
 	dposv_(&UPLO, &N, &NRHS, krn_chd, &LDA, wt, &LDB, &info);
+
+	return info;
+}
+
+int gpr_predict(double *yp, const double *wt, const double *krnp, unsigned long np, const unsigned long ns)
+{
+	unsigned char tr;
+	int N, M, LDA, incx, incy;
+	double alph, bet;
+
+	tr = 'T';
+	M = (int)ns;
+	N = (int)np;
+	LDA = M;
+	incx = 1;
+	incy = 1;
+	alph = 1.0;
+	bet = 0;
+
+	dgemv_(&tr, &M, &N, &alph, krnp, &LDA, wt, &incx, &bet, yp, &incy);
+
+	return 0;
+}
+
+int get_var_mat(double *var, double *krnpp, double *krnp, double *krn, unsigned long np, unsigned long ns)
+{
+	unsigned char tra, trb, UPLO;
+	int N, M, NRHS, LDA, LDB, LDC, info;
+	unsigned long i, j, k;
+	double *V, alph, bet, tmp, eps;
+
+	V = malloc(np * ns * sizeof(double));
+	assert(V);
+
+	eps = 1E-7;
+
+	for (i = 0; i < ns; i++) {
+		krn[i * ns + i] += eps;
+	}
+
+	for (i = 0; i < ns * np; i++) {
+		V[i] = krnp[i];
+	}
+
+	UPLO = 'L';
+	N = ns;
+	NRHS = np;
+	LDA = N;
+	LDB = N;
+
+	dposv_(&UPLO, &N, &NRHS, krn, &LDA, V, &LDB, &info);
+	assert(info == 0);
+
+	for (i = 0; i < np * np; i++) {
+		var[i] = krnpp[i];
+	}
+
+	N = ns;
+	M = np;
+	LDA = N;
+	LDB = N;
+	LDC = M;
+	alph = -1.0;
+	bet = 1.0;
+	tra = 'T';
+	trb = 'N';
+
+	dgemm_(&tra, &trb, &M, &M, &N, &alph, krnp, &LDA, V, &LDB, &bet, var, &LDC);
+
+	free(V);
 
 	return info;
 }
--- lib_gpr.h
+++ lib_gpr.h
@@ -1,3 +1,13 @@
+extern void dposv_(const unsigned char *UPLO, const int *N, const int *NRHS, double *A, const int *LDA, double *B, const int *LDB, int *info);
+extern void dpotrs_(const unsigned char *UPLO, const int *N, const int *NRHS, double *A, const int *LDA, double *B, const int *LDB, int *info);
+extern void dgemm_(const unsigned char *transa, const unsigned char *transb, const int *m, const int *n,
+		   const int *k, const double *alpha, const double *a, const int *lda, const double *b,
+		   const int *ldb, const double *beta, double *c, const int *ldc);
+extern void dgemv_(const unsigned char *transa, const int *m, const int *n, const double *alpha,
+		   const double *a, const int *lda, const double *x, const int *incx, const double *beta,
+		   double *y, const int *incy);
 double fit_gpr(double *y, double *x, unsigned long ns, unsigned long dim, double *krn);
-int get_krn_se(double *krn, const double *x, const double *xp, unsigned long ns, unsigned long dim, const double *p, int np);
+int get_krn_se(double *krn, const double *x, const double *xp, unsigned long nx, unsigned long nxp, unsigned long dim, const double *p, int npar);
 int get_gpr_weights(double *wt, double *krn_chd, const double *krn, unsigned long ns, unsigned long dim, const double *y);
+int gpr_predict(double *yp, const double *wt, const double *krnp, unsigned long np, const unsigned long ns);
+int get_var_mat(double *var, double *krnpp, double *krnp, double *krn_chd, unsigned long np, unsigned long ns);
