cb7bc3022acca34ccf74814348fe6b4c35954af8
sarath srinivas s <srinix@protonmail.com>
2019-02-06 15:34:30 +0530

    Moved lapack and blas declarations to atlas headers.

--- lib_gpr.h
+++ lib_gpr.h
@@ -9,25 +9,6 @@ struct gpr_dat {
 	double *r2;
 };
 
-extern void dposv_(const unsigned char *UPLO, const int *N, const int *NRHS, double *A, const int *LDA, double *B, const int *LDB, int *info);
-extern void dpotrs_(const unsigned char *UPLO, const int *N, const int *NRHS, double *A, const int *LDA, double *B, const int *LDB, int *info);
-extern void dtrsm_(const unsigned char *SIDE, const unsigned char *UPLO, const unsigned char *TRA,
-		   const unsigned char *DIAG, const int *M, const int *N, const double *ALPHA,
-		   const double *A, const int *LDA, double *B, const int *LDB);
-extern void dgemm_(const unsigned char *transa, const unsigned char *transb, const int *m, const int *n,
-		   const int *k, const double *alpha, const double *a, const int *lda, const double *b,
-		   const int *ldb, const double *beta, double *c, const int *ldc);
-extern void dgemv_(const unsigned char *transa, const int *m, const int *n, const double *alpha,
-		   const double *a, const int *lda, const double *x, const int *incx, const double *beta,
-		   double *y, const int *incy);
-extern double ddot_(const int *N, const double *X, const int *incx, const double *Y, const int *incy);
-extern double dsyrk_(const unsigned char *UPLO, const unsigned char *TRA, const int *N, const int *K,
-		     const double *ALPHA, const double *A, const int *LDA, const double *BETA, double *C,
-		     const int *LDC);
-extern void dsymv_(const unsigned char *uplo, const int *n, const double *alpha,
-		   const double *a, const int *lda, const double *x, const int *incx, const double *beta,
-		   double *y, const int *incy);
-
 void get_krn_se(double *krn, const double *x, const double *xp, unsigned long nx, unsigned long nxp, unsigned long dim, const double *p, int npar);
 void get_krn_se_ard(double *krn, const double *x, const double *xp, unsigned long nx, unsigned long nxp, unsigned long dim, const double *p, int npar);
 void get_gpr_weights(double *wt, double *krn_chd, const double *krn, unsigned long ns, unsigned long dim, const double *y);
