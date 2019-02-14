e76c1c54d6e9e898062ddbc0b14ee310c37362c3
sarath srinivas s <srinix@protonmail.com>
2019-02-15 01:39:26 +0530

    Moved covar to covar.c and llhd optim to hyperopt.c

--- lib_gpr.h
+++ lib_gpr.h
@@ -10,7 +10,6 @@ struct gpr_dat {
 };
 
 void get_krn_se(double *krn, const double *x, const double *xp, unsigned long nx, unsigned long nxp, unsigned long dim, const double *p, int npar);
-void get_krn_se_ard(double *krn, const double *x, const double *xp, unsigned long nx, unsigned long nxp, unsigned long dim, const double *p, int npar);
 void get_gpr_weights(double *wt, double *krn_chd, const double *krn, unsigned long ns, unsigned long dim, const double *y);
 void gpr_predict(double *yp, const double *wt, const double *krnp, unsigned long np, const unsigned long ns);
 void get_var_mat(double *var, double *krnpp, double *krnp, double *krn_chd, unsigned long np, unsigned long ns);
@@ -19,9 +18,15 @@ void get_var_mat_chd(double *var, double *krnpp, double *krnp, double *krn_chd, 
 void get_hyper_param(double *p, int np, double *x, double *y, unsigned long ns, int dim);
 void get_hyper_param_ard(double *p, int np, double *x, double *y, unsigned long ns, int dim);
 void gpr_interpolate(double *y, double *x, unsigned long ns, unsigned int dim, double *xp, unsigned long np, double *yp, double *p, unsigned int npar, double *var_yp);
+
 /* COVARIANCE FUNCTIONS */
+void get_krn_se_ard(double *krn, const double *x, const double *xp, unsigned long nx, unsigned long nxp, unsigned long dim, const double *p, int npar);
+void get_dkrn_se_ard(double *dK, int k, const double *x, const double *kxx, unsigned long nx, unsigned int dim, const double *p, int np);
 void get_krn_rat_quad(double *krn, const double *x, const double *xp, unsigned long nx, unsigned long nxp, unsigned long dim, const double *par, int npar, const double *hpar, int nhpar);
+
 /* TESTS */
+double test_get_dkrn_se_ard(unsigned int m, unsigned int dim, unsigned long nx, double eps, int seed);
+double test_jac_cost_fun_ard(int m, unsigned int dim, unsigned long nx, double eps, int seed);
 double test_gpr_interpolate(unsigned long ns, unsigned long np, int fno, int seed);
 
 
