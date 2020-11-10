f1269450d1319c84fffda2b9071e36e6b190772c
sarath <srinix@protonmail.com>
2020-11-10 16:14:05 +0530

    covar and tests with OOP design.

--- covar.py
+++ covar.py
@@ -1,248 +1,238 @@
 import torch as tc
-import numpy as np
+from torch import Tensor
+from typing import List
 
 tc.set_default_tensor_type(tc.DoubleTensor)
-
 tc.set_printoptions(precision=7, sci_mode=True)
 
 
-def dist_square(x, xs=None):
+class Covar():
+    """
+     Base class for covariance kernels for Gaussian process
+     regression.
+    """
 
-    x = x.view((-1, x.shape[-2], x.shape[-1]))
+    def __init__(self) -> None:
+        self.hyper_param: Tensor = NotImplemented
 
-    x2 = tc.sum(x.square(), 2)
+    def hyper_param_size(self, x: Tensor) -> int:
+        raise NotImplementedError
 
-    if xs is None:
+    def kernel(self, x: Tensor, xp: Tensor = None, hp: Tensor = None)\
+            -> Tensor:
+        raise NotImplementedError
 
-        sqd = -2.0 * tc.matmul(x, x.transpose(1, 2)) + x2.unsqueeze(2).add(
-            x2.unsqueeze(1))
-
-    else:
-        xs = xs.view((-1, xs.shape[-2], xs.shape[-1]))
-
-        xs2 = tc.sum(xs.square(), 2)
-
-        sqd = -2.0 * tc.matmul(xs, x.transpose(1, 2)) + xs2.unsqueeze_(2).add(
-            x2.unsqueeze_(1))
-
-        xs.squeeze_(0)
-
-    x.squeeze_(0)
-
-    return sqd.squeeze(0)
+    def kernel_and_grad(self, x: Tensor, hp: Tensor = None) ->  \
+            List[Tensor]:
+        raise NotImplementedError
 
 
-def sq_exp_noise(x, xs=None, hp=None, deriv=False, **kargs):
+class Squared_exponential(Covar):
+    '''
+     Squared exponential covariance K(x,x') = exp(-|x-x'|^2)
+    '''
 
-    x = x.view((-1, x.shape[-2], x.shape[-1]))
+    def __init__(self) -> None:
+        self.hyper_param: Tensor = NotImplemented
 
-    dim = x.shape[-1]
-    nc = x.shape[0]
+    def hyper_param_size(self, x: Tensor) -> int:
+        return x.shape[-1] + 2
 
-    if hp is None:
-        hp = tc.ones([nc, dim + 2])
-        hp[:, 1].fill_(1E-4)
+    def distance(self, x: Tensor, xp: Tensor = None) -> Tensor:
+        x = x.view((-1, x.shape[-2], x.shape[-1]))
+
+        x2 = tc.sum(x.square(), 2)
+
+        if xp is None:
+
+            sqd = -2.0 * tc.matmul(x, x.transpose(1, 2)) \
+                 + x2.unsqueeze(2).add(x2.unsqueeze(1))
+
+        else:
+            xp = xp.view((-1, xp.shape[-2], xp.shape[-1]))
+
+            xp2 = tc.sum(xp.square(), 2)
+
+            sqd = -2.0 * tc.matmul(xp, x.transpose(1, 2)) \
+                + xp2.unsqueeze_(2).add(x2.unsqueeze_(1))
+
+            xp.squeeze_(0)
+
+        x.squeeze_(0)
+
+        return sqd.squeeze(0)
+
+    def kernel(self, x: Tensor, xp: Tensor = None, hp: Tensor = None) \
+            -> Tensor:
+
+        x = x.view((-1, x.shape[-2], x.shape[-1]))
+
+        dim = x.shape[-1]
+        nc = x.shape[0]
+
+        if hp is None:
+            hp = tc.ones([nc, dim + 2])
+            hp[:, 1].fill_(1E-4)
+            self.hyper_param = hp.squeeze_(0)
+        else:
+            self.hyper_param = hp
+
+        if hp.dim() == 1:
+            hp = hp.view((nc, -1))
+
+        assert hp.dim() == 2
+
+        sig = hp[:, 0]
+        sig_noise = hp[:, 1]
+        ls = hp[:, 2:]
+        eps = sig_noise.square()
+        ls.unsqueeze_(1)
+        xl = x.mul(ls)
+
+        if xp is None:
+            sqd = self.distance(xl)
+
+            sqd = sqd.view((-1, sqd.shape[-2], sqd.shape[-1]))
+
+            sqd.mul_(-1.0)
+            sqd.exp_()
+            sqd.mul_(sig[:, None, None].square())
+
+            idt = tc.empty_like(sqd).copy_(tc.eye(sqd.shape[-1]))
+            idt.mul_(eps[:, None, None])
+            sqd.add_(idt)
+
+        else:
+            xp = xp.view((-1, xp.shape[-2], xp.shape[-1]))
+            xpl = xp.mul(ls)
+
+            sqd = self.distance(xl, xp=xpl)
+            sqd = sqd.view((-1, sqd.shape[-2], sqd.shape[-1]))
+
+            sqd.mul_(-1.0)
+            sqd.exp_()
+            sqd.mul_(sig[:, None, None].square())
+
         hp.squeeze_(0)
-        return hp
-    elif not tc.is_tensor(hp):
-        hp = tc.tensor(hp)
-    else:
-        pass
 
-    if hp.dim() == 1:
-        hp = hp.view((nc, -1))
+        sqd.squeeze_(0)
 
-    assert hp.dim() == 2
-
-    sig = hp[:, 0]
-    sig_noise = hp[:, 1]
-    ls = hp[:, 2:]
-    eps = sig_noise.square()
-    ls.unsqueeze_(1)
-    xl = x.mul(ls)
-
-    if xs is None:
-        sqd = dist_square(xl)
-
-        sqd = sqd.view((-1, sqd.shape[-2], sqd.shape[-1]))
-
-        sqd.mul_(-1.0)
-        sqd.exp_()
-        sqd.mul_(sig[:, None, None].square())
-
-        idt = tc.empty_like(sqd).copy_(tc.eye(sqd.shape[-1]))
-        idt.mul_(eps[:, None, None])
-        sqd.add_(idt)
-
-    else:
-        xs = xs.view((-1, xs.shape[-2], xs.shape[-1]))
-        xsl = xs.mul(ls)
-
-        sqd = dist_square(xl, xs=xsl)
-        sqd = sqd.view((-1, sqd.shape[-2], sqd.shape[-1]))
-
-        sqd.mul_(-1.0)
-        sqd.exp_()
-        sqd.mul_(sig[:, None, None].square())
-
-    hp.squeeze_(0)
-
-    sqd.squeeze_(0)
-
-    if deriv == False:
         return sqd
-    else:
-        return sqd, d_sq_exp_noise(x, sqd, hp)
 
+    def kernel_and_grad(self, x: Tensor, hp: Tensor = None) \
+            -> List[Tensor]:
 
-def d_sq_exp_noise(x, krn, hp):
+        krn = self.kernel(x, hp=hp)
 
-    x = x.view((-1, x.shape[-2], x.shape[-1]))
-    krn = krn.view((-1, krn.shape[-2], krn.shape[-1]))
+        x = x.view((-1, x.shape[-2], x.shape[-1]))
+        krn = krn.view((-1, krn.shape[-2], krn.shape[-1]))
 
-    nc = x.shape[0]
-    nhp = hp.shape[-1]
-    n = krn.shape[-1]
+        if hp is None:
+            hp = self.hyper_param
 
-    if hp.dim() == 1:
-        hp = hp.view((nc, -1))
+        nc = x.shape[0]
+        nhp = hp.shape[-1]
+        n = krn.shape[-1]
 
-    assert hp.dim() == 2
+        if hp.dim() == 1:
+            hp = hp.view((nc, -1))
 
-    dkrn = tc.empty([nc, nhp, n, n])
+        assert hp.dim() == 2
 
-    sig = hp[:, 0]
-    sig_noise = hp[:, 1]
-    ls = hp[:, 2:]
-    eps = sig_noise.square()
+        dkrn = tc.empty([nc, nhp, n, n])
 
-    idt = tc.empty_like(krn).copy_(tc.eye(n))
-    krn_noise = idt.mul(eps[:, None, None])
-    krn = krn.sub(krn_noise)
+        sig = hp[:, 0]
+        sig_noise = hp[:, 1]
+        ls = hp[:, 2:]
+        eps = sig_noise.square()
 
-    dkrn[:, 0, :, :] = krn.mul(sig[:, None, None].reciprocal().mul_(2.0))
-    dkrn[:, 1, :, :] = idt.mul_(sig_noise[:, None, None].mul(2.0))
+        idt = tc.empty_like(krn).copy_(tc.eye(n))
+        krn_noise = idt.mul(eps[:, None, None])
+        krn = krn.sub(krn_noise)
 
-    xt = x.transpose(-2, -1)
-    diff = xt[:, :, :, None].sub(xt[:, :, None, :])
+        dkrn[:, 0, :, :] = krn.mul(sig[:, None, None].reciprocal().mul_(2.0))
+        dkrn[:, 1, :, :] = idt.mul_(sig_noise[:, None, None].mul(2.0))
 
-    diff.square_()
-    diff.mul_(ls[:, :, None, None])
-    diff.mul_(krn[:, None, :, :])
-    diff.mul_(-2.0)
+        xt = x.transpose(-2, -1)
+        diff = xt[:, :, :, None].sub(xt[:, :, None, :])
 
-    dkrn[:, 2:, :, :] = diff
+        diff.square_()
+        diff.mul_(ls[:, :, None, None])
+        diff.mul_(krn[:, None, :, :])
+        diff.mul_(-2.0)
 
-    x.squeeze_(0)
-    hp.squeeze_(0)
-    dkrn.squeeze_(0)
+        dkrn[:, 2:, :, :] = diff
 
-    return dkrn
-
-
-def sq_exp(x, xs=None, hp=None, deriv=False, **kargs):
-
-    x = x.view((-1, x.shape[-2], x.shape[-1]))
-
-    dim = x.shape[-1]
-    nc = x.shape[0]
-
-    if hp is None:
-        hp = tc.ones([nc, dim + 1])
+        x.squeeze_(0)
         hp.squeeze_(0)
-        return hp
-    elif not tc.is_tensor(hp):
-        hp = tc.tensor(hp)
-    else:
-        pass
+        krn.squeeze_(0)
+        dkrn.squeeze_(0)
 
-    if hp.dim() == 1:
-        hp = hp.view((nc, -1))
-
-    assert hp.dim() == 2
-
-    sig = hp[:, 0]
-    ls = hp[:, 1:]
-    eps = 1e-7 * tc.ones(nc)
-    ls.unsqueeze_(1)
-    xl = x.mul(ls)
-
-    if xs is None:
-        sqd = dist_square(xl)
-
-        sqd = sqd.view((-1, sqd.shape[-2], sqd.shape[-1]))
-
-        sqd.mul_(-1.0)
-        sqd.exp_()
-        sqd.mul_(sig[:, None, None].square())
-
-        idt = tc.empty_like(sqd).copy_(tc.eye(sqd.shape[-1]))
-        idt.mul_(eps[:, None, None])
-        sqd.add_(idt)
-
-    else:
-        xs = xs.view((-1, xs.shape[-2], xs.shape[-1]))
-        xsl = xs.mul(ls)
-
-        sqd = dist_square(xl, xs=xsl)
-        sqd = sqd.view((-1, sqd.shape[-2], sqd.shape[-1]))
-
-        sqd.mul_(-1.0)
-        sqd.exp_()
-        sqd.mul_(sig[:, None, None].square())
-
-    hp.squeeze_(0)
-
-    sqd.squeeze_(0)
-
-    if deriv == False:
-        return sqd
-    else:
-        return sqd, d_sq_exp(x, sqd, hp)
+        return [krn, dkrn]
 
 
-def d_sq_exp(x, krn, hp):
+# Docs here
 
-    x = x.view((-1, x.shape[-2], x.shape[-1]))
-    krn = krn.view((-1, krn.shape[-2], krn.shape[-1]))
+Covar.kernel.__doc__ = """
+Covariance kernel matrix for batched samples x and test samples xp.
 
-    nc = x.shape[0]
-    nhp = hp.shape[-1]
-    n = krn.shape[-1]
+Parameters:
+    x: Tensor[..., n, dim]
+        Batched Training samples.
+    xp: Tensor[..., m, dim]
+        Batched Test samples
+    hp: Tensor[..., nhp]
+        Batched Hyperparameter of the covariance,
 
-    if hp.dim() == 1:
-        hp = hp.view((nc, -1))
-
-    assert hp.dim() == 2
-
-    dkrn = tc.empty([nc, nhp, n, n])
-
-    sig = hp[:, 0]
-    ls = hp[:, 1:]
-
-    eps = 1e-7 * tc.ones(nc)
-    idt = tc.empty_like(krn).copy_(tc.eye(n))
-    idt.mul_(eps[:, None, None])
-    krn = krn.sub(idt)
-
-    dkrn[:, 0, :, :] = krn.mul(sig[:, None, None].reciprocal().mul_(2.0))
-
-    xt = x.transpose(-2, -1)
-
-    diff = xt[:, :, :, None].sub(xt[:, :, None, :])
-
-    diff.square_()
-    diff.mul_(ls[:, :, None, None])
-    diff.mul_(krn[:, None, :, :])
-    diff.mul_(-2.0)
-
-    dkrn[:, 1:, :, :] = diff
-
-    x.squeeze_(0)
-    hp.squeeze_(0)
-    dkrn.squeeze_(0)
-
-    return dkrn
+Returns:
+    krn_mat: Tensor[..., m, n]
+                Covariance kernel matrix K(x,x')
+"""
 
 
-covars = {'sq_exp': sq_exp, 'sq_exp_noise': sq_exp_noise}
+Covar.kernel_and_grad.__doc__ = """
+Derivative of the covariance kernel wrt hyperparameters.
+    dK(x,x) / d\theta_i
+
+Parameters:
+    x: Tensor[..., n, dim]
+        Batched training samples
+    hp: Tensor[..., nhp]
+        Batched hyperparameters at which the derivative is taken.
+        if None, the object internal Covar.hyper_parameter is used.
+
+Returns:
+    dkrn: Tensor[..., nhp, n, n]
+          Batched matrix derivative wrt each hyperparameter.
+"""
+
+
+Covar.hyper_param_size.__doc__ = """
+Gives the number of hyperparameters for this covariance kernel.
+
+Parameters:
+    x: Tensor[..., n, dim]
+        Batched training samples. Only the dim value is used.
+
+Returns:
+    nhp: int
+         The number of hyperparameters.
+"""
+
+
+Squared_exponential.distance.__doc__ = """
+Euclidean distance matrix between x and xp.
+
+Parameters:
+    x: Tensor[..., n, dim]
+        Batched train samples
+    xp: Tensor[..., m, dim]
+        Batched test samples
+
+N.B: Only one of x or xp allowed to be batched.
+
+Returns:
+    sqd: Tensor[..., m, n]
+        Distance matrix between x and xp
+        if xp = None then returns distance matrix of x.
+"""
--- test_covar.py
+++ test_covar.py
@@ -1,56 +1,59 @@
 import torch as tc
-import numpy as np
-import scipy as scp
-from .gpr import log_likelihood, jac_log_likelihood
-from .covar import covars
+
+# from gpr import log_likelihood, jac_log_likelihood
+from .covar import Squared_exponential, Covar
 from itertools import product
 import pytest as pyt
 
 tc.set_default_tensor_type(tc.DoubleTensor)
 
 n = (10, 100, 1000)
+dim = (2, 5)
 
-tparams = list(product(n, covars))
+sq_exp = Squared_exponential()
+
+covars = (sq_exp,)
+
+tparams = list(product(covars, n, dim))
 
 
-@pyt.mark.parametrize("n,covar_fun", tparams)
-def test_covar_symmetric(n, covar_fun, tol=1e-7):
-    dim = 5
-    cov = covars[covar_fun]
+@pyt.mark.parametrize("cov, n, dim", tparams)
+def test_covar_symmetric(cov: Covar, n: int, dim: int, tol: float = 1e-7) \
+        -> None:
     x = tc.rand([n, dim])
-    hp = tc.rand(cov(x).shape[0])
-    krn = cov(x, hp=hp)
+    hp = tc.rand([cov.hyper_param_size(x)])
+    krn = cov.kernel(x, hp=hp)
 
     assert tc.allclose(krn, krn.t(), atol=tol)
 
+    return None
 
-@pyt.mark.parametrize("n,covar_fun", tparams)
-def test_covar_posdef(n, covar_fun, tol=1e-7):
-    dim = 5
-    cov = covars[covar_fun]
+
+@pyt.mark.parametrize("cov, n, dim", tparams)
+def test_covar_posdef(cov: Covar, n: int, dim: int, tol=1e-7) -> None:
     x = tc.rand([n, dim])
-    hp = tc.rand(cov(x).shape[0])
-    krn = cov(x, hp=hp)
+    hp = tc.rand([cov.hyper_param_size(x)])
+    krn = cov.kernel(x, hp=hp)
     eig = tc.eig(krn)[0][:, 0]
 
     assert tc.all(eig > 0)
 
+    return None
 
-@pyt.mark.parametrize("n,covar_fun", tparams)
-def test_covar_batch(n, covar_fun):
+
+@pyt.mark.parametrize("cov, n, dim", tparams)
+def test_covar_batch(cov: Covar, n: int, dim: int) -> None:
     nc = 4
-    dim = 5
-    cov = covars[covar_fun]
     xb = tc.rand(nc, n, dim)
-    nhp = cov(xb[0, :, :]).shape[-1]
+    nhp = cov.hyper_param_size(xb)
     hpb = tc.rand(nc, nhp)
 
-    krn_batch, dkrn_batch = cov(xb, hp=hpb, deriv=True)
+    krn_batch, dkrn_batch = cov.kernel_and_grad(xb, hp=hpb)
 
-    krn1, dkrn1 = cov(xb[0, :, :], hp=hpb[0, :], deriv=True)
-    krn2, dkrn2 = cov(xb[1, :, :], hp=hpb[1, :], deriv=True)
-    krn3, dkrn3 = cov(xb[2, :, :], hp=hpb[2, :], deriv=True)
-    krn4, dkrn4 = cov(xb[3, :, :], hp=hpb[3, :], deriv=True)
+    krn1, dkrn1 = cov.kernel_and_grad(xb[0, :, :], hp=hpb[0, :])
+    krn2, dkrn2 = cov.kernel_and_grad(xb[1, :, :], hp=hpb[1, :])
+    krn3, dkrn3 = cov.kernel_and_grad(xb[2, :, :], hp=hpb[2, :])
+    krn4, dkrn4 = cov.kernel_and_grad(xb[3, :, :], hp=hpb[3, :])
 
     krn = tc.stack((krn1, krn2, krn3, krn4), dim=0)
     dkrn = tc.stack((dkrn1, dkrn2, dkrn3, dkrn4), dim=0)
@@ -58,16 +61,17 @@ def test_covar_batch(n, covar_fun):
     assert tc.allclose(krn_batch, krn)
     assert tc.allclose(dkrn_batch, dkrn)
 
+    return None
 
-@pyt.mark.parametrize("n,covar_fun", tparams)
-def test_covar_deriv(n, covar_fun, eps_diff=1e-5):
-    dim = 5
-    cov = covars[covar_fun]
+
+@pyt.mark.parametrize("cov, n, dim", tparams)
+def test_covar_deriv(cov: Covar, n: int, dim: int, eps_diff: float = 1e-5) \
+        -> None:
     x = tc.rand(n, dim)
-    nhp = cov(x).shape[-1]
+    nhp = cov.hyper_param_size(x)
     hp = tc.rand(nhp)
 
-    krn, dkrn = cov(x, hp=hp, deriv=True)
+    krn, dkrn = cov.kernel_and_grad(x, hp=hp)
 
     dkrn_diff = tc.ones_like(dkrn)
 
@@ -76,15 +80,17 @@ def test_covar_deriv(n, covar_fun, eps_diff=1e-5):
         eps[k] = eps_diff
         hp_eps = hp.add(eps)
 
-        krn = cov(x, hp=hp)
-        krn_eps = cov(x, hp=hp_eps)
+        krn = cov.kernel(x, hp=hp)
+        krn_eps = cov.kernel(x, hp=hp_eps)
 
         dkrn_diff[k, :, :] = krn_eps.sub_(krn).div_(eps_diff)
 
-    # return dkrn, dkrn_diff
     assert tc.allclose(dkrn, dkrn_diff, atol=eps_diff)
 
+    return None
 
+
+"""
 @pyt.mark.parametrize("n,covar_fun", tparams)
 def test_jac_likelihood(n, covar_fun, eps_diff=1e-5):
     dim = 5
@@ -109,3 +115,4 @@ def test_jac_likelihood(n, covar_fun, eps_diff=1e-5):
         jac_llhd_diff[k] = llhd_eps.sub_(llhd).div_(eps_diff)
 
     assert tc.allclose(jac_llhd, jac_llhd_diff, atol=1e-3)
+"""
