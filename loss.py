72262b84a169689ef521431daf2865cf846b6cbe
srinix <srinix@protonmail.com>
2020-11-16 18:00:15 +0530

    Added loss class and tests.

---
+++ loss.py
@@ -0,0 +1,135 @@
+import torch as tc
+import numpy as np
+from numpy import ndarray
+import opt_einsum as oes
+from typing import Tuple
+from .gpr import GPR
+
+tc.set_default_tensor_type(tc.DoubleTensor)
+
+
+class Loss():
+    """
+     Base class for cost functions for Gaussian process model
+     selection
+    """
+    def __init__(self, model: GPR) -> None:
+        self.model: GPR = model
+        return None
+
+    def loss(self, params: ndarray) -> float:
+        raise NotImplementedError
+
+    def grad(self, params: ndarray) -> ndarray:
+        raise NotImplementedError
+
+    def loss_and_grad(self, params: ndarray) -> Tuple[float, ndarray]:
+        raise NotImplementedError
+
+
+class MLE(Loss):
+    """
+     Log Marginal Likelihood for hyperparameters.
+    """
+    def loss(self, params: ndarray) -> float:
+        self.model.set_params(tc.from_numpy(params))
+
+        krn = self.model.cov.kernel(self.model.x)
+        krnchd = tc.cholesky(krn)
+
+        y = self.model.y
+
+        wt = tc.squeeze(tc.cholesky_solve(y.reshape(-1, 1), krnchd))
+
+        llhd = 0.5 * tc.dot(wt, y) + tc.sum(tc.log(
+            tc.diag(krnchd))) + 0.5 * len(y) * tc.log(tc.tensor(2 * np.pi))
+
+        return llhd.numpy()
+
+    def grad(self, params: ndarray) -> ndarray:
+        self.model.set_params(tc.from_numpy(params))
+
+        krn, dkrn = self.model.cov.kernel_and_grad(self.model.x)
+        krnchd = tc.cholesky(krn)
+
+        y = self.model.y
+
+        wt = tc.cholesky_solve(y.reshape(-1, 1), krnchd).squeeze_()
+        kk = tc.cholesky_solve(dkrn, krnchd)
+
+        # tr1 = tc.tensordot(dkrn, wt[:, None].mul(wt[None, :]), dims=2)
+        tr1 = oes.contract('i,kij,j->k', wt, dkrn, wt, backend='torch')
+        tr2 = tc.diagonal(kk, dim1=-1, dim2=-2).sum(-1)
+
+        jac_llhd = tr1.sub_(tr2).mul_(-0.5)
+
+        return jac_llhd.numpy()
+
+    def loss_and_grad(self, params: ndarray) -> Tuple[float, ndarray]:
+        self.model.set_params(tc.from_numpy(params))
+
+        krn, dkrn = self.model.cov.kernel_and_grad(self.model.x)
+        krnchd = tc.cholesky(krn)
+
+        y = self.model.y
+
+        wt = tc.squeeze(tc.cholesky_solve(y.reshape(-1, 1), krnchd))
+
+        llhd = 0.5 * tc.dot(wt, y) + tc.sum(tc.log(
+            tc.diag(krnchd))) + 0.5 * len(y) * tc.log(tc.tensor(2 * np.pi))
+
+        kk = tc.cholesky_solve(dkrn, krnchd)
+
+        tr1 = tc.tensordot(dkrn, wt[:, None].mul(wt[None, :]), dims=2)
+        tr2 = tc.diagonal(kk, dim1=-1, dim2=-2).sum(-1)
+
+        jac_llhd = tr1.sub_(tr2).mul_(-0.5)
+
+        return (llhd.numpy(), jac_llhd.numpy())
+
+
+# Docs hereafter
+
+Loss.loss.__doc__ = """
+Computes value of loss at the model parameters.
+
+Parameters
+----------
+params: ndarray[nhp,]
+   Parameters of the model at which the loss function is evaluated.
+
+Returns
+-------
+float
+    Value of the loss function.
+"""
+
+Loss.grad.__doc__ = """
+Computes gradient of loss at the model parameters.
+
+Parameters
+----------
+params: ndarray[nhp,]
+   Parameter of the model at which the gradient of the loss function\
+   is evaluated.
+
+Returns
+-------
+ndarray[nhp,]
+    Gradient of the loss function wrt model parameters.
+"""
+
+Loss.loss_and_grad.__doc__ = """
+Computes loss function and its gradient at the model parameters.
+
+Parameters
+----------
+params: ndarray[nhp,]
+   Parameter of the model at which the loss function and its gradient\
+   are evaluated.
+
+Returns
+-------
+tuple(float, ndarray[nhp,])
+    Loss function and its gradient wrt model parameters.
+"""
---
+++ test_loss.py
@@ -0,0 +1,46 @@
+import torch as tc
+import numpy as np
+from .gpr import Exact_GP
+from .covar import Squared_exponential
+from .loss import MLE
+from itertools import product
+import pytest as pyt
+
+tc.set_default_tensor_type(tc.DoubleTensor)
+
+n = (10, 100, 1000)
+dim = (2, 3, 5, 7)
+
+tparams = list(product(n, dim))
+
+
+@pyt.mark.parametrize("n, dim", tparams)
+def test_grad(n: int, dim: int, eps_diff: float = 1e-5) -> None:
+    x = tc.rand([n, dim])
+    y = tc.exp(-x.square().sum(1))
+
+    cov = Squared_exponential(x)
+
+    mod = Exact_GP(x, y, cov)
+
+    loss = MLE(mod)
+
+    params = tc.rand_like(mod.params).numpy()
+
+    grad = loss.grad(params)
+
+    grad_diff = np.zeros_like(grad)
+
+    for k in range(0, params.shape[-1]):
+        eps = np.zeros_like(params)
+        eps[k] = eps_diff
+
+        val = loss.loss(params)
+        params_eps = params + eps
+        val_eps = loss.loss(params_eps)
+
+        grad_diff[k] = (val_eps - val) / eps_diff
+
+    assert np.allclose(np.log10(np.abs(grad)),
+                       np.log10(np.abs(grad_diff)),
+                       atol=1e-3)
