91263ef4c4efa42751651d0ab1f5d652d24d758f
sarath <srinix@protonmail.com>
2021-01-15 23:41:14 +0530

    Hyperparameter quadratic update.

---
+++ hp_update.py
@@ -0,0 +1,26 @@
+import torch as tc
+from torch import Tensor
+from .loss import Loss
+
+
+def update_model(loss: Loss, y: Tensor, eps: float) -> None:
+    """
+     Update GPR model hyperparameters with new sample y ~ eps*y_old
+    """
+
+    loss.model.y = tc.clone(y)
+    old_params = tc.clone(loss.model.params).numpy()
+
+    f0, J = loss.loss_and_grad(old_params)
+
+    fp = loss.loss(old_params + eps * J)
+    fm = loss.loss(old_params - eps * J)
+
+    C1 = (fp + fm) / (2.0 * eps)
+    C2 = (fp + fm - 2 * f0) / (2.0 * eps ** 2)
+
+    gamma = -0.5 * (C1 / C2)
+
+    new_params = old_params - gamma * J
+
+    loss.model.set_params(tc.from_numpy(new_params))
