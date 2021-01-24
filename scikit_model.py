093bc8126573767aab9c6ef505d0c0b3723770c8
sarath <srinix@protonmail.com>
2021-01-24 14:49:28 +0530

    Wrap instant of GPR.

--- scikit_model.py
+++ scikit_model.py
@@ -12,32 +12,24 @@ T_OPT = Callable[..., Opt]
 T_COVAR = Callable[..., Covar]
 
 
-class SK_GPR(RegressorMixin, BaseEstimator):
+class SK_WRAP(RegressorMixin, BaseEstimator):
     """
      Scikit model wrapper for GPR
     """
 
-    def __init__(
-        self, model: T_GPR, covar: T_COVAR, loss: T_LOSS, opt: T_OPT
-    ) -> None:
-        self.model: T_GPR = model
-        self.loss: T_LOSS = loss
-        self.opt: T_OPT = opt
-        self.covar: Covar = covar
-        # self.mod: GPR = NotImplemented
+    def __init__(self, model: GPR) -> None:
+        self.model: GPR = model
         return None
 
     def fit(self, x: Tensor, y: Tensor) -> Any:
         print("Fitting")
-        self.mod = self.model(x, y, self.covar())
-        loss = self.loss(self.mod)
-        opt = self.opt(loss)
-
-        opt.minimize()
+        self.model.x = x
+        self.model.y = y
 
         return self
 
     def predict(self, xp: Tensor) -> Tensor:
         print("Predicting")
-        yp, covar = self.mod.predict(xp, var="none")
+        self.need_upd = True
+        yp, covar = self.model.predict(xp, var="none")
         return yp
